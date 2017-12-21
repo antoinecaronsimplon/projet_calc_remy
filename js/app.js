61ff56afb0debfc826501a06e5fde04d7c01013e
AntoineCARON <antoinecaron.simplon@gmail.com>
2017-12-21 16:50:49 +0100

    progress

---
+++ js/app.js
@@ -0,0 +1,43 @@
+// function(){	
+// 	$("#result").click(function(){
+// 		console.log("coucou fred");
+
+// 	});
+
+	
+
+	// function clickButtonFormulaire(){
+		
+	// }
+
+	// function calculer(nb1, nb2, operation){
+		
+	// 	}
+
+	// 	return res;
+	// }
+
+
+	// function add(a, b){
+		
+	// }	
+
+	// function substract(a, b){
+		
+	// }	
+
+	// function multiply(a, b){
+		
+	// }	
+
+	// function divide(a, b){
+		
+	// }	
+
+// };
+
+
+
+function run1(){
+	document.form.resulat.value += "1";
+}
